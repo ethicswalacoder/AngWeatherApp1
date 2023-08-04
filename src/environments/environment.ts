95f671835066d095ab4cb42f4ebec94b9bb45e71
Sahoo <artbangla21@gmail.com>
2023-08-04 15:32:09 +0530

    complete the apiFeatching using fetchAPI and nex i have to bind the data.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '5baef9db1emsh404dc00e921d899p1eff00jsn80b6bb0abaac',
+
+
+
+};
